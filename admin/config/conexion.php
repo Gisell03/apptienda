ff41259f29866ddd2b7cf4e098be405f94c0f574
Gisell03 <102748916+Gisell03@users.noreply.github.com>
2022-10-13 10:56:34 -0500

    Update conexion.php

--- admin/config/conexion.php
+++ admin/config/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("b7ebj5x4vmkrzjzxbsyb-mysql.services.clever-cloud.com", "uq7biqqrwvtadijqroot", "TOlllRkGcFgCy9TtKkdz", "b7ebj5x4vmkrzjzxbsyb");
+$conn = new mysqli("bo2wnmemhht6qlef8myd-mysql.services.clever-cloud.com", "uzbswv1jpihja46d", "rWzPAU89Pw0WSDYeV7B4", "bo2wnmemhht6qlef8myd");
   
   if ($conn->connect_error) {
     die("ERROR: No se puede conectar al servidor: " . $conn->connect_error);
