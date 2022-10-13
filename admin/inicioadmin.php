90d3139716f3d4a840f8e035b5eb147f1879ffdf
Gisell03 <102748916+Gisell03@users.noreply.github.com>
2022-10-14 01:00:22 +0200

    Update inicioadmin.php

--- admin/inicioadmin.php
+++ admin/inicioadmin.php
@@ -3,7 +3,7 @@
             <div class="col-md-12">
             <div class="jumbotron">
              <h1 class="display-3">Bienvenido</h1>
-                <p class="lead">Zona Administrativa</p>
+                <p class="lead">Depositamos nuestra confianza y aplicativo en usted.</p>
                 <hr class="my-2">
             </div>
             </div>
